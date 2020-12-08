81f7d00af111574deab07b35369fabe23e99d397
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-08 23:20:25 +0200

    Make a simple UITableViewController which have a list of texts of images names

---
+++ Project01/Project1/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Usama Fouad on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
