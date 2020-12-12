4c031052478bb85cd4e80fa2ec0ebee591769bdc
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-12 17:01:31 +0200

    Check if user select the correct flag or not, if yes, increase the score, decrease the score otherwise, all this appear in an alert message

--- Project02/Project2/Project2/ViewController.swift
+++ Project02/Project2/Project2/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -38,5 +38,24 @@ class ViewController: UIViewController {
         
         title = countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
