b1f61c604664e442f0bedf2ef01cc0df547d8e79
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-12 13:59:47 +0200

    Initialize the game with its images and required button and navigation controller

---
+++ Project02/Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Usama Fouad on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
