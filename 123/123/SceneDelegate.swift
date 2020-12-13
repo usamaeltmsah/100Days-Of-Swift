cdacf62d8623846e39f272756ad8304e8606e6c3
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-13 20:35:48 +0200

    Let the app support iOS 12

--- 123/123/SceneDelegate.swift
+++ 123/123/SceneDelegate.swift
@@ -1,19 +1,17 @@
 //
 //  SceneDelegate.swift
-//  Project2
+//  123
 //
-//  Created by Usama Fouad on 12/12/20.
+//  Created by Usama Fouad on 12/13/20.
 //
 
 import UIKit
 
-@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
-    @available(iOS 13.0, *)
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -33,19 +31,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
-    @available(iOS 13.0, *)
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
-    @available(iOS 13.0, *)
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
 
-    @available(iOS 13.0, *)
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
---
+++ 123/123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Usama Fouad on 12/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
