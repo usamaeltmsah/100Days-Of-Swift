3b240edf24252766555c8515f2024221a8c34bf3
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-14 23:20:30 +0200

    Initialize the DetailViewController class

---
+++ Projects 1-3 Consolidation/Projects 1-3 Consolidation/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Projects 1-3 Consolidation
+//
+//  Created by Usama Fouad on 12/14/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
