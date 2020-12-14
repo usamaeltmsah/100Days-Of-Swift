cf11ffa530899e1f9f11e283630865d44708d3a6
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-14 23:08:22 +0200

    Read the flags from the content folder

--- Projects 1-3 Consolidation/Projects 1-3 Consolidation/ViewController.swift
+++ Projects 1-3 Consolidation/Projects 1-3 Consolidation/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var flags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix(".png") {
+                flags.append(item)
+            }
+        }
+        
+        flags.sort()
+        print(flags.count)
     }
 
 
