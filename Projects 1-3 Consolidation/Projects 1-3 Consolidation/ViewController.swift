43aae2445855ca9d46fb59773102b30633026858
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-16 01:08:49 +0200

    Use the defined label and imageView for the cell

--- Projects 1-3 Consolidation/Projects 1-3 Consolidation/ViewController.swift
+++ Projects 1-3 Consolidation/Projects 1-3 Consolidation/ViewController.swift
@@ -8,26 +8,13 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
-//    var flags = [String]()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         title = "Flags of the world"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-//        let fm = FileManager.default
-//        let path = Bundle.main.resourcePath!
-//        let items = try! fm.contentsOfDirectory(atPath: path)
-//        for item in items {
-//            if item.hasSuffix(".png") {
-//                flags.append(item)
-//            }
-//        }
-//
-//        flags.sort()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,19 +22,15 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath) as! TableViewCell
         
-        cell.imageView?.layer.cornerRadius = (cell.imageView?.bounds.width)! / 4
-        
-        cell.imageView?.addBorders(width: 1.5, color: UIColor.black.cgColor)
-        
-        cell.textLabel?.text = countryDict[sortedKeys[indexPath.row]]
+        cell.countryName?.text = countryDict[sortedKeys[indexPath.row]]
         
         let image = UIImage(named: sortedKeys[indexPath.row].lowercased())
+
+        let resized_image = image?.scaleImage(toSize: CGSize(width: 70, height: 50))
         
-        let resized_image = image?.scaleImage(toSize: CGSize(width: 30, height: 15))
-        
-        cell.imageView?.image = resized_image
+        cell.flagImgView?.image = resized_image
         return cell
     }
     
