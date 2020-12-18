b6c0dd0461aafe8038bd72e750674d70bf4da79e
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-18 22:44:05 +0200

    Add a plus sign to the right of the navigation bar, which the user will click on to get a text field to enter his answer

--- Project05/Project5/Project5/ViewController.swift
+++ Project05/Project5/Project5/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -42,6 +43,23 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = userdWords[indexPath.row]
         
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 
 }
