c0b79ee212662a740fb26e8d272625cbeaab68ca
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-18 23:54:54 +0200

    Implement the isReal method

--- Project05/Project5/Project5/ViewController.swift
+++ Project05/Project5/Project5/ViewController.swift
@@ -86,7 +86,10 @@ class ViewController: UITableViewController {
     }
 
     func isReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
     }
 }
 
