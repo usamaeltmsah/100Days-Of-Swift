5657772e021a6aac5213b7fc1cdd704ca1889079
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-18 21:20:14 +0200

    Load the start.txt file into allWords array, which contains a 12,000 which will be used in the game

--- Project05/Project5/Project5/ViewController.swift
+++ Project05/Project5/Project5/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var allWords = [String]()
+    var userdWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["Silkworm"]
+        }
     }
 
 
