6f98cfc56223551945ca30ef9edc1a1fbac5cd8d
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-18 20:40:41 +0200

    Initialize Project 5

---
+++ Project05/Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Usama Fouad on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
