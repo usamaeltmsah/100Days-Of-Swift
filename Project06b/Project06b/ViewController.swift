9ebf521887956828b4c62cb8692c49f1183520e0
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-21 00:22:21 +0200

    Remove too much repetition in the code!

--- Project06b/Project06b/ViewController.swift
+++ Project06b/Project06b/ViewController.swift
@@ -13,34 +13,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let label1 = UILabel()
-        label1.translatesAutoresizingMaskIntoConstraints = false
         label1.backgroundColor = .red
         label1.text = "THESE"
-        label1.sizeToFit()
         
         let label2 = UILabel()
-        label2.translatesAutoresizingMaskIntoConstraints = false
         label2.backgroundColor = .cyan
         label2.text = "ARE"
-        label2.sizeToFit()
         
         let label3 = UILabel()
-        label3.translatesAutoresizingMaskIntoConstraints = false
+        
         label3.backgroundColor = .yellow
         label3.text = "SOME"
-        label3.sizeToFit()
         
         let label4 = UILabel()
-        label4.translatesAutoresizingMaskIntoConstraints = false
         label4.backgroundColor = .green
         label4.text = "AWESOME"
-        label4.sizeToFit()
         
         let label5 = UILabel()
-        label5.translatesAutoresizingMaskIntoConstraints = false
         label5.backgroundColor = .orange
         label5.text = "LABELS"
-        label5.sizeToFit()
         
         view.addSubview(label1)
         view.addSubview(label2)
@@ -61,6 +52,8 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.sizeToFit()
             label.textAlignment = .center
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
