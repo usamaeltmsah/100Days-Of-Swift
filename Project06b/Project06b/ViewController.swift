56be86f55631ef04463b8da14f4851c97d97be41
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-20 22:36:59 +0200

    Give the 5 labels constrains to appear on the screen in more good format

--- Project06b/Project06b/ViewController.swift
+++ Project06b/Project06b/ViewController.swift
@@ -47,6 +47,14 @@ class ViewController: UIViewController {
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
+        
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
     }
 
 
