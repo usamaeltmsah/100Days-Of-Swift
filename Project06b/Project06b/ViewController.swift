838271244e4b52555fcc33ef852406b6d5830304
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-21 01:50:32 +0200

    Remove redundant constraints

--- Project06b/Project06b/ViewController.swift
+++ Project06b/Project06b/ViewController.swift
@@ -51,13 +51,12 @@ class ViewController: UIViewController {
             label.translatesAutoresizingMaskIntoConstraints = false
             label.sizeToFit()
             label.textAlignment = .center
+            
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
 
             label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 1/5, constant: -10).isActive = true
             
-            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 10).isActive = true
             if let previous = previous {
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
             } else {
