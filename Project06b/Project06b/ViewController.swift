5e98e89a72e35eba6264e3f6063ac32e718cef7c
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-20 22:06:17 +0200

    Initialize Project06b

---
+++ Project06b/Project06b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project06b
+//
+//  Created by Usama Fouad on 20/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
