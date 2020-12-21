a9da9d678d3786e9177d645faef96d7442e6dcb3
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-21 21:55:58 +0200

    Let it be a TableViewController, and add + navigation item in the top right of the navigation bar

--- Projects 4-6 Consolidation/Projects 4-6 Consolidation/ViewController.swift
+++ Projects 4-6 Consolidation/Projects 4-6 Consolidation/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
     }
-
 
 }
 
