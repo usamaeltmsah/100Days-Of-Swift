b937f10ffa234cec3ac4135658fc7711729fe5fa
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-21 21:57:11 +0200

    Implement numberOfRowsInSection and cellForRowAt methods

--- Projects 4-6 Consolidation/Projects 4-6 Consolidation/ViewController.swift
+++ Projects 4-6 Consolidation/Projects 4-6 Consolidation/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        
+        return cell
+    }
 
 }
 
