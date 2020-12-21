42606b0ed7935f3a15ea6807aa56c7fd570afb52
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-21 21:12:23 +0200

    Initialize Projects 4-6 consolidation project

---
+++ Projects 4-6 Consolidation/Projects 4-6 Consolidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects 4-6 Consolidation
+//
+//  Created by Usama Fouad on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
