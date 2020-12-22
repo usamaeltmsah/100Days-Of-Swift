c110c501583bbcc1d5fab28e20c3aa4c116cd293
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-22 20:29:18 +0200

    Initialize project 7

---
+++ Project07/Project07/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project07
+//
+//  Created by Usama Fouad on 22/12/2020.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
