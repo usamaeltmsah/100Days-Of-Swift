37961a9562c96a7510c8edbc3f1f4b80edd92f75
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-22 22:45:10 +0200

    Initialize DetailViewController with a webkit and the detailItem

---
+++ Project07/Project07/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewCellViewController.swift
+//  Project07
+//
+//  Created by Usama Fouad on 22/12/2020.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
