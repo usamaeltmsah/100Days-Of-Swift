a10afcd061f942b85cbc1c0fdfa52dd419db2c6b
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-22 20:39:49 +0200

    Implement numberOfRowsInSection and cellForRowAt methods

--- Project07/Project07/ViewController.swift
+++ Project07/Project07/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var petition = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petition.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Title goes here!"
+        cell.detailTextLabel?.text = "Subtitle goes here!"
+        
+        return cell
+    }
 
 }
 
