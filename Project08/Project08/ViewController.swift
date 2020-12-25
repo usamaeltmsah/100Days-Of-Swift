03be3a390d146b0dee9027ae4cc80b177bfbb13f
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-25 23:46:22 +0200

    Add constraints for the submit and clear buttons

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -85,6 +85,14 @@ class ViewController: UIViewController {
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),
+            
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+            clear.heightAnchor.constraint(equalToConstant: 44),
         ])
         
         cluesLabel.backgroundColor = .red
