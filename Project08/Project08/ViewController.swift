d195527d7f27dd33656d6150926abca76b7a2852
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-26 00:00:25 +0200

    Add Buttons to the ButtonsView which player will click to select the string in it

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -110,6 +110,32 @@ class ViewController: UIViewController {
             buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
         
+        // set some values for the width and height of each button
+        let width = 150
+        let height = 80
+
+        // create 20 buttons as a 4x5 grid
+        for row in 0..<4 {
+            for col in 0..<5 {
+                // create a new button and give it a big font size
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+
+                // give the button some temporary text so we can see it on-screen
+                letterButton.setTitle("WWW", for: .normal)
+
+                // calculate the frame of this button using its column and row
+                let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+
+                // add it to the buttons view
+                buttonsView.addSubview(letterButton)
+
+                // and also to our letterButtons array
+                letterButtons.append(letterButton)
+            }
+        }
+        
         cluesLabel.backgroundColor = .red
         answersLabel.backgroundColor = .blue
         buttonsView.backgroundColor = .green
