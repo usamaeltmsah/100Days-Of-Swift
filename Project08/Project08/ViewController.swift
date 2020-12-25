6f24668f0e1c2f904b297308aa92c41f4a0bee60
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-25 22:07:58 +0200

    Create five properties to store the important parts of our user interface

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
