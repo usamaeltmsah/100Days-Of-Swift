1b5205ceaab89894b605510c45765e5287652f2d
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-25 21:48:22 +0200

    Initialize Project 8

---
+++ Project08/Project08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project08
+//
+//  Created by Usama Fouad on 24/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
