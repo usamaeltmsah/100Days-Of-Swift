176a70b1d6892ad56cde329c74f7160d7a9a048c
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-26 22:06:56 +0200

    Load the data for game's level from the file

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -146,7 +146,7 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            loadLevel()
     }
     
     @objc func letterTapped(_ sender: UIButton) {
@@ -159,6 +159,33 @@ class ViewController: UIViewController {
     
     @objc func clearTapped(_ sender: UIButton) {
         
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFileUrl = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            if let levelContents = try? String(contentsOf: levelFileUrl) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
     }
 
 
