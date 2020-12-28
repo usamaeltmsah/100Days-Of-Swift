5c3d16a00133ec6db0c81c30c1ae1cf645e486b8
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-28 22:02:09 +0200

    Initialize the Projects 7-9 Consolidation files

---
+++ Projects 7-9 Consolidation/Projects 7-9 Consolidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects 7-9 Consolidation
+//
+//  Created by Usama Fouad on 28/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
