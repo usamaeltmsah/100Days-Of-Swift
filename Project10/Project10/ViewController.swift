df6e5c27713f05c8d0830587fe59f91f9f4f7a60
Usama Fouad <usamaeltmsah@gmail.com>
2020-12-30 23:29:53 +0200

    Implement cellForItemAt method, with dummy data

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -17,5 +17,15 @@ class ViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell")
+        }
+        
+        cell.name?.text = "This is ....... Cell Number ....... \(indexPath.row)"
+        
+        return cell
+    }
 }
 
