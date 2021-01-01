08f000243960441e2d2790e1848a37d82c65231d
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-01 19:26:30 +0200

    Add a Physics for the box

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -23,6 +25,7 @@ class GameScene: SKScene {
         let location = touch.location(in: self)
         
         let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
         
         box.position = location
         addChild(box)
