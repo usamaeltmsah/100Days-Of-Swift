8b040899a892c60e7bf4fb0fd57b9c6285964f34
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-02 22:23:53 +0200

    Get a copy from project 10 as project 12

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Usama Fouad on 30/12/2020.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String = ""
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Usama Fouad on 30/12/2020.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
