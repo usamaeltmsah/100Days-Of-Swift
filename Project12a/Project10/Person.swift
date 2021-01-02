7a7e335f30106d9858c823a31a52d97c08f576a8
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-02 23:05:10 +0200

    Apply NSCoder to encode and decode Person objects

--- Project12a/Project10/Person.swift
+++ Project12a/Project10/Person.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
     var name: String = ""
     var image: String
     
     init(name: String, image: String) {
         self.name = name
         self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
     }
 }
--- Project12a/Project10/ViewController.swift
+++ Project12a/Project10/ViewController.swift
@@ -66,6 +66,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unkown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
         
         dismiss(animated: true)
@@ -100,6 +101,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
                 return
             }
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .default))
@@ -115,6 +117,13 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .default))
         present(ac, animated: true)
+    }
+    
+    func save() {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
     }
 }
 
