ceb4006464d64ef160acb1aae8ea2955ef01d94d
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-04 21:52:39 +0200

    Add a navigation button to take an image for a thing

--- Projects10-12 Consolidation/Projects10-12 Consolidation/ViewController.swift
+++ Projects10-12 Consolidation/Projects10-12 Consolidation/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate,  U
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(addNewThing))
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
