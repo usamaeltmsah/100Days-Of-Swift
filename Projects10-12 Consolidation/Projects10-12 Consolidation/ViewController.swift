52b4f5ebafca5aa73764b24044761d9f32b70657
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-04 14:18:59 +0200

    Initialize Projects10-12 Consolidation project

---
+++ Projects10-12 Consolidation/Projects10-12 Consolidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects10-12 Consolidation
+//
+//  Created by Usama Fouad on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
