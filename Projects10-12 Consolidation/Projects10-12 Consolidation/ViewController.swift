99a6b8a74ac29e4d610a6c9361e1ab01fdc79676
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-04 19:58:46 +0200

    Implement the numberOfRowsInSection method

--- Projects10-12 Consolidation/Projects10-12 Consolidation/ViewController.swift
+++ Projects10-12 Consolidation/Projects10-12 Consolidation/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var things = [ThingCell]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return things.count
+    }
 
 }
 
