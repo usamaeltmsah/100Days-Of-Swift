e0720346a6db011f4f264a4254e3bcb03f74be47
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-04 19:57:02 +0200

    Make a ThingCell class which holds the attributes for each cell in the tableView

---
+++ Projects10-12 Consolidation/Projects10-12 Consolidation/ThingCell.swift
@@ -0,0 +1,14 @@
+//
+//  ThingCell.swift
+//  Projects10-12 Consolidation
+//
+//  Created by Usama Fouad on 04/01/2021.
+//
+
+import UIKit
+
+class ThingCell: UITableViewCell {
+    @IBOutlet var thingImage: UIImageView!
+    @IBOutlet var captionLabel: UILabel!
+
+}
