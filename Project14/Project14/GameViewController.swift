55f99c2a596eaecd3f102550a64107dbbdd0e013
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-10 21:30:04 +0200

    Initialize project14 files

---
+++ Project14/Project14/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewController.swift
+//  Project14
+//
+//  Created by Usama Fouad on 10/01/2021.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
