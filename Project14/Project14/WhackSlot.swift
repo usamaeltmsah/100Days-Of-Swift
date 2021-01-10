9df8cbb03dd5adb6c3ecb4a660cdc56e931be3cd
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-10 23:05:02 +0200

    Show good or enemy penguins randomly

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
+    
+    var isVisible = false
+    var isHit = false
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -27,5 +31,21 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hidTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
     }
 }
