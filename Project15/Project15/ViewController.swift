15d2d933736439d233f017318efbf0a04351ff29
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-11 23:48:00 +0200

    animate method on the UIView

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
     
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: []) {
+                switch self.currentAnimation {
+                case 0:
+                    break
+                default:
+                    break
+            }
+        } completion: { finished in
+            sender.isHidden = false
+        }
+
         currentAnimation += 1
         
         if currentAnimation > 7 {
