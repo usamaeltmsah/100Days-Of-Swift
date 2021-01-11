e58053b36b31f10557c1993e5d72b38e2a911df3
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-12 00:11:35 +0200

    Add animations to the penguin!

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -22,16 +22,33 @@ class ViewController: UIViewController {
     @IBAction func tapped(_ sender: UIButton) {
         sender.isHidden = true
         
-        UIView.animate(withDuration: 1, delay: 0, options: []) {
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
                 switch self.currentAnimation {
                 case 0:
+                    self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                     break
+                case 1:
+                    self.imageView.transform = .identity
+                case 2:
+                    self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                case 3:
+                    self.imageView.transform = .identity
+                case 4:
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                case 5:
+                    self.imageView.transform = .identity
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = .green
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = .clear
                 default:
                     break
+                }
+            }) { finished in
+                sender.isHidden = false
             }
-        } completion: { finished in
-            sender.isHidden = false
-        }
 
         currentAnimation += 1
         
