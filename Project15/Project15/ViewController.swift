6ab12fc27f1b1c86396f6fa2d677538ef38f71d8
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-11 23:36:09 +0200

    Add a tap button and penguin image to the view

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    @IBOutlet var tapped: NSLayoutConstraint!
+    
 }
 
