943552e337fcddb12b3c4badf95518ec7e1f6733
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-12 21:05:23 +0200

    Initialize the Consolidation Projects 13-15 files

---
+++ Projects13-15-Consolidation/Projects13-15-Consolidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects13-15-Consolidation
+//
+//  Created by Usama Fouad on 12/01/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
