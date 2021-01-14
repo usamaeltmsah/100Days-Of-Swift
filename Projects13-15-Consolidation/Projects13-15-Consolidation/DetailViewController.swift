c1a6386f4f294079d03593fdea1d31353e7f116f
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-14 23:04:58 +0200

    Initialize the DetilalViewController class

---
+++ Projects13-15-Consolidation/Projects13-15-Consolidation/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Projects13-15-Consolidation
+//
+//  Created by Usama Fouad on 13/01/2021.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
