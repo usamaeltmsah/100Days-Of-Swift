95fa99f5bba8797d431c6ae3e91877e0c0d24b96
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-14 22:39:28 +0200

    Fetch json data (Get countries data from the given api)

--- Projects13-15-Consolidation/Projects13-15-Consolidation/ViewController.swift
+++ Projects13-15-Consolidation/Projects13-15-Consolidation/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var countries = [Country]()
+    var allCountries = [Country]()
+    var filteredCountries = [Country]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let filterButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForText))
+        
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshPage))
+        
+        navigationItem.leftBarButtonItems = [refreshButton, filterButton]
+        performSelector(inBackground: #selector(fetchJson), with: nil)
     }
+    
+    @objc func fetchJson() {
+        let urlString: String
+        
+        urlString = "https://restcountries.eu/rest/v2/all"
 
-
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+    }
 }
 
