efef239e211e118ec3ddc1a205362f39ced9685f
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-15 21:39:35 +0200

    Define the MapView in the ViewController class

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -5,10 +5,12 @@
 //  Created by Usama Fouad on 15/01/2021.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
