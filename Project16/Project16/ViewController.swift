28972872af8bf6743a06565e9672665359151a14
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-15 22:10:18 +0200

    Show info about country as an alert

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -46,6 +46,18 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         return annotaionView
     }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard  let capital = view.annotation as? Capital else { return }
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
+    }
 
 
 }
