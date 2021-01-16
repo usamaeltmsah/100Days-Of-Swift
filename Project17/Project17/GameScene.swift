8406a61462d5f33afb8f91e6e76f4ebdb36e2851
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-16 22:37:09 +0200

    Create enemies to collide with the player!

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -12,6 +12,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
     
+    var possipleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer?
+    var isGameOver = false
+    
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
@@ -44,6 +48,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = .zero // Or: CGVector(dx: 0, dy: 0)
         // Set current game scene to be the contact delegate of the physics world
         physicsWorld.contactDelegate = self
+        
+        // timeInterval: 0.35 -> Three times a second
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+    
+    @objc func createEnemy() {
+        guard let enemy = possipleEnemies.randomElement() else { return }
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        // Collide with the player
+        sprite.physicsBody?.categoryBitMask = 1
+        // Speed of moving (From the other side)
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        // Spin while flying
+        sprite.physicsBody?.angularVelocity = 5
+        // Never slow down.
+        sprite.physicsBody?.linearDamping = 0
+        // Never stop spinning (constant rate spinning)
+        sprite.physicsBody?.angularDamping = 0
     }
     
     override func update(_ currentTime: TimeInterval) {
