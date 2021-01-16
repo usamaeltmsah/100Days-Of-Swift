a5f35214a4b25140a129c0dd4969f96aa139dbe9
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-16 22:04:22 +0200

    Add score label to the game

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -33,6 +33,11 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
         player.physicsBody?.contactTestBitMask = 1
         addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
     }
     
     override func update(_ currentTime: TimeInterval) {
