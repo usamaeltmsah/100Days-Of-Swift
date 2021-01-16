fd2b99b4b8e72203dcdad841a265c0e78a970ad1
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-16 21:56:22 +0200

    Add a dynamic background to screen

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -19,7 +19,19 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
+        backgroundColor = .black
         
+        
+        starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        addChild(player)
     }
     
     override func update(_ currentTime: TimeInterval) {
