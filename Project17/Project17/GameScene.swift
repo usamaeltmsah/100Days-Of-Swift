522ae6fe2f4b3eb1a0b2ecac700dadf77ff44694
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-16 21:34:01 +0200

    Initialize project 17 (Space Racer game)

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Usama Fouad on 16/01/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
