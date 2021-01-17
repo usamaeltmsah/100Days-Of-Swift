8d2da4c8ff839cc93ea1903be5801bfce2f4db7d
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-17 22:09:18 +0200

    Initialize project 17

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Usama Fouad on 17/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
