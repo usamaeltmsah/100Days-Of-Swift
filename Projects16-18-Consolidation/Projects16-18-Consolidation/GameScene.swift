5e0c4fc7ba124528134a826f04f7895113b1798b
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-18 22:03:39 +0200

    Allow sniper to be movable

--- Projects16-18-Consolidation/Projects16-18-Consolidation/GameScene.swift
+++ Projects16-18-Consolidation/Projects16-18-Consolidation/GameScene.swift
@@ -9,6 +9,7 @@ import SpriteKit
 class GameScene: SKScene {
     var background: SKSpriteNode!
     var sniper: SKSpriteNode!
+    var isSniperTouched = false
     
     override func didMove(to view: SKView) {
         background = SKSpriteNode(imageNamed: "background")
@@ -20,5 +21,33 @@ class GameScene: SKScene {
         sniper = SKSpriteNode(imageNamed: "sniper")
         sniper.position = CGPoint(x: 512, y: 384)
         addChild(sniper)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        if nodes(at: location).contains(sniper) {
+            isSniperTouched = true
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        var location = touch.location(in: self)
+        
+        if location.y < 150 {
+            location.y = 150
+        } else if location.y > 618 {
+            location.y = 618
+        }
+        
+        if isSniperTouched {
+            sniper.position = location
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isSniperTouched = false
     }
 }
