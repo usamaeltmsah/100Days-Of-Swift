53c540068cc62baf28a5876edb7f7770cad3e2c2
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-18 02:48:07 +0200

    Add comments to help in debugging!

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -12,20 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /// 1. Assertions
+            //  Assertions are removed when we build our apps for the App Store.
+        
 //        let errMsg = "Math Failure!"
 //        assert(1 == 1, errMsg)
 //        assert(1 == 2, errMsg)
         
-        // Breakpoint with a condition
+        // 2. Breakpoint with a condition
+        // fn + F6 => To carry on execution
+        // (Ctrl+Cmd+Y) => Continue executing my program until you hit another breakpoint
+        
         for i in 1...100 {
             print("Got number \(i).")
         }
         
-        // Try use Exception Break point => It will help you find your bug easily
+        // 3. Exception Break point => It will help you find your bug easily
             // In the left Navigator in Xcode
                 // Select "Show the Breakpoint navigator item" from the top list
                     // Or just click [CMD + 8] as a shortcut
                 // Then choose from the left button + icon ...
+        /// 4. Interactive LLDB (lldb) debugger window => where you can type commands to query values and run methods
+        
+        /// 5. Capture View Hierarchy (3D representation)
+        /*
+         Debug menu > Choose View Debugging -> Capture View Hierarchy (In the Debugging area at the buttom)
+         */
     }
 
 
