765aa6ac16aa402a52a363f6860f9c579e59e51f
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-18 23:04:01 +0200

    Add 3 rows to have the targets on

--- Projects16-18-Consolidation/Projects16-18-Consolidation/GameScene.swift
+++ Projects16-18-Consolidation/Projects16-18-Consolidation/GameScene.swift
@@ -12,6 +12,9 @@ class GameScene: SKScene {
     var isSniperTouched = false
     
     var levelTimerLabel: SKLabelNode!
+    
+    let goodTargets = ["target3", "target4"]
+    let dangerousTargets = ["dangerous_target1", "dangerous_target2"]
 
     var levelTimerValue: Int = 60 {
         didSet {
@@ -39,6 +42,8 @@ class GameScene: SKScene {
         levelTimerLabel.text = "Time left: \(levelTimerValue)"
         addChild(levelTimerLabel)
         
+        drawRows()
+        
         countDown()
     }
     
@@ -54,6 +59,24 @@ class GameScene: SKScene {
         let sequence = SKAction.sequence([wait, block])
 
         run(SKAction.repeatForever(sequence), withKey: "countdown")
+    }
+    
+    func createRow(size: CGSize, color: UIColor, pos: CGPoint) -> SKSpriteNode {
+        let row = SKSpriteNode(color: color, size: size)
+        row.position = pos
+            
+        return row
+    }
+    
+    func drawRows() {
+        let size = CGSize(width: 800, height: 5)
+        let color: UIColor = .white
+        
+        for i in -1...1 {
+            let row = createRow(size: size, color: color, pos: CGPoint(x: centerPoint.x, y: centerPoint.y + CGFloat(i)*150.0))
+            
+            addChild(row)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
