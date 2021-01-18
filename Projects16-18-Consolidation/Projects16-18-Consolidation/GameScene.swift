39399e7953c2ea5f363b6983eafc891da6c64009
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-18 20:42:26 +0200

    Initialize Projects16-18-Consolidation project

---
+++ Projects16-18-Consolidation/Projects16-18-Consolidation/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Projects16-18-Consolidation
+//
+//  Created by Usama Fouad on 18/01/2021.
+//
+
+import SpriteKit
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
