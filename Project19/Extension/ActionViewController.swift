6379266fd6741ee8270af7d6707ff7dc59f8c63e
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-20 22:46:35 +0200

    Get data from the parent app to the extension

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -14,6 +14,21 @@ class ActionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // extensionContext: lets us control how it interacts with the parent app.
+        // inputItems: is an array of data the parent app is sending to our extension to use.
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            // Our input item contains an array of attachments.
+            if let itemProvider = inputItem.attachments?.first {
+                // loadItem(forTypeIdentifier:): Ask the item provider to actually provide us with its item.
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {
+                    // dict: dictionary that was given to us by the item provider.
+                    // error: any error that occurred.
+                    [weak self] (dict, error) in
+                    // Do stuff
+                }
+            }
+        }
     }
 
     @IBAction func done() {
