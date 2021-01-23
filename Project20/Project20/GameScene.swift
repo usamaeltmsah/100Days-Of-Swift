5ed885a1337ae526df454e06752d75f37edbd094
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-23 23:15:19 +0200

    Add properties of the game

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -8,6 +8,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameTimer: Timer?
+    var fireWorks = [SKNode]()
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score = 0 {
+        didSet {
+            
+        }
+    }
+    
     override func didMove(to view: SKView) {
         
     }
