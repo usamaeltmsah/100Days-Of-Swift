86e25dded73fb165a818c09736796f618cf99840
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-23 22:40:25 +0200

    Initialize Project 20 game and add the files

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Usama Fouad on 23/01/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
