9501a2bd8ec7fa36f68a2f0b89ca9860e32c4508
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-24 22:46:57 +0200

    Add some important helpful notes to the scheduleLocal() method

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -31,22 +31,31 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        // This method will configure all the data needed to schedule a notification, which is three things:-
+        // content (what to show), a trigger (when to show it), and a request (the combination of content and trigger.)
+        
         let center = UNUserNotificationCenter.current()
         
+        // UNMutableNotificationContent: What to show
         let content = UNMutableNotificationContent()
         content.title = "Late wake up call"
         content.body = "The early bird catches the worm, but the second mouse gets the cheese."
         
+        // categoryIdentifier: attach custom actions.
         content.categoryIdentifier = "alarm"
+        // userInfo dictionary: To attach custom data to the notification, e.g. an internal ID, use the.
         content.userInfo = ["customData": "fizzbuzz"]
+        // MARK: UNNotificationSound object: User if you want to specify a sound.
         content.sound = .default
         
+        // Repeat the notification every morning at 10:30am
 //        var dateComponents = DateComponents()
 //        dateComponents.hour = 10
 //        dateComponents.minute = 30
 //        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
-        // Wait 5 seconds
+        // MARK: center.removeAllPendingNotificationRequests(): Used to cancel pending notifications – i.e., notifications you have scheduled that have yet to be delivered because their trigger hasn’t been met.
+        // Wait 5 seconds then show the notification
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
