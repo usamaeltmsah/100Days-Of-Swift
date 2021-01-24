831ec680a206c05e3c47344ebeb0f8ba7327f2f7
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-24 21:39:43 +0200

    Add two bar button items one for register and another for schedule

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+    
+    @objc func registerLocal() {
+        
+    }
+    
+    @objc func scheduleLocal() {
+        
     }
 
 
