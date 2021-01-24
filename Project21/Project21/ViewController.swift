c85d4098fa9ab67c66ba54eb3110d8a7add39d2a
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-24 22:17:43 +0200

    Implement scheduleLocal() method, so schedule a notification after 5 seconds

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -31,7 +31,25 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
         
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+//        var dateComponents = DateComponents()
+//        dateComponents.hour = 10
+//        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        // Wait 5 seconds
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 
 
