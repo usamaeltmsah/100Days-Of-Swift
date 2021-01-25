aa615836829b7b97bae8b24e180411bca3f64ff4
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-25 22:51:58 +0200

    Add a Note struct, which have the Note name, and its context

---
+++ Projects19-21 Consolidation/Projects19-21 Consolidation/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Projects19-21 Consolidation
+//
+//  Created by Usama Fouad on 25/01/2021.
+//
+
+import UIKit
+
+struct Note: Codable {
+    var name: String
+    var context: String
+}
