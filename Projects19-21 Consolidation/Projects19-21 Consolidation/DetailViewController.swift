3b703d7e576daefd32327d06e2c0e3ae49c78943
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-25 23:23:58 +0200

    Add a textView to the DetailViewController and fix the bug of not scrolling in the textView

--- Projects19-21 Consolidation/Projects19-21 Consolidation/DetailViewController.swift
+++ Projects19-21 Consolidation/Projects19-21 Consolidation/DetailViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var textView: UITextView!
+    
     var note: Note?
     
     override func viewDidLoad() {
@@ -16,6 +17,30 @@ class DetailViewController: UIViewController {
         
         title = note?.name
         navigationItem.largeTitleDisplayMode = .never
+        
+        let notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        textView.scrollIndicatorInsets = textView.contentInset
+        
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
     }
 
 }
