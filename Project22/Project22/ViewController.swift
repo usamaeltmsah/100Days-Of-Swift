557c0d0464d6ff29f5fbaf794d8d6cd011cff332
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-27 22:26:40 +0200

    Request always access location

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -5,13 +5,24 @@
 //  Created by Usama Fouad on 27/01/2021.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var distanceReading: UILabel!
+    // Core Location class that lets us configure how we want to be notified about location, and will also deliver location updates to us.
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        // requestAlwaysAuthorization(): if you have already been granted location permission then things will Just Work; if you haven't, iOS will request it now.
+        // requestWhenInUseAuthorization(): When in use location permission.
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
 
 
