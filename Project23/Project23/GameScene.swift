cd3068b1858e286a8fc92a9e02e5d37e6a3fc4d3
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-28 14:21:36 +0200

    Initialize project 23 (Swifty Ninja)

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Usama Fouad on 28/01/2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
