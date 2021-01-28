037fd0cbbf9fb6e32fabfd8a1417cf0c9cc9885a
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-28 22:49:07 +0200

    Destroy the bomb when the player touches it

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -170,8 +170,33 @@ class GameScene: SKScene {
                 run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
             } else if node.name == "bomb" {
                 // Destroy the bomb
+                guard let bombContainer = node.parent as? SKSpriteNode else { continue }
+                
+                if let emitter = SKEmitterNode(fileNamed: "sliceHitBomb") {
+                    emitter.position = bombContainer.position
+                    addChild(emitter)
+                }
+                
+                node.name = ""
+                bombContainer.physicsBody?.isDynamic = false
+                
+                let scaleOut = SKAction.scale(to: 0.001, duration: 02)
+                let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+                let group = SKAction.group([scaleOut, fadeOut])
+                let seq = SKAction.sequence([group, .removeFromParent()])
+                bombContainer.run(seq)
+                
+                if let index = activeEnemies.firstIndex(of: bombContainer) {
+                    activeEnemies.remove(at: index)
+                }
+                run(SKAction.playSoundFileNamed("explosion.caf", waitForCompletion: false))
+                endGame()
             }
         }
+    }
+    
+    func endGame() {
+        
     }
     
     func playSwooshingSound() {
