7dfda672e45e81f63d571f417839e71353097e6f
Usama Fouad <usamaeltmsah@gmail.com>
2021-01-30 22:44:18 +0200

    Extend Array so that it has a mutating remove(item:) method. If the item exists more than once, it should remove only the first instance it finds

---
+++ Projects-22-24-Consolidation.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+
+extension UIView {
+    // Use animation to scale its size down to 0.0001 over a specified number of seconds.
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration, animations: {
+            self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        })
+    }
+}
+
+// Extend Int with a times() method that runs a closure as many times as the number is high.
+extension Int {
+    func times(closure: () -> Void) {
+        for _ in 0..<self {
+            closure()
+        }
+    }
+}
+
+5.times {
+    print("Hello!")
+}
+
+// Extend Array so that it has a mutating remove(item:) method. If the item exists more than once, it should remove only the first instance it finds
+extension Array where Element: Comparable {
+    mutating func remove(item: Element) {
+        if let index = self.firstIndex(of: item) {
+            self.remove(at: index)
+        }
+    }
+}
+
+
+var numbers = [1, 2, 3, 4, 5]
+numbers.remove(item: 3)
