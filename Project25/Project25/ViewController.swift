b0596df5c529bb7123a9d71d272a9020b2d11df0
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-01 03:00:40 +0200

    Design the UI

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
