0800c60e118ee12a217dadc409279fb92c1b930f
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-01 22:11:34 +0200

    Initialize 3 important methods (didReceive stream, didStartReceivingResourceWithName, and didFinishReceivingResourceWithName)

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -75,6 +75,18 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
     }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        <#code#>
+    }
 
 
 }
