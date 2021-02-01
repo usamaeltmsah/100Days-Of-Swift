d844706c2311515279560d2bed34f4f4ad3208a4
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-01 03:12:00 +0200

    Add configurations of collection view (Number of items should be shown, and how it should be)

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var images = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Selfi Camera"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
     }
 
 
