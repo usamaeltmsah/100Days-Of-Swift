daad91fc8f684ee11c367cf4f2d2da188c80b4dd
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-02 23:09:20 +0200

    Add a background to the game

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -16,6 +16,12 @@ enum CollisionTypes: UInt32 {
 }
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        
+        addChild(background)
         loadLevel()
     }
     
