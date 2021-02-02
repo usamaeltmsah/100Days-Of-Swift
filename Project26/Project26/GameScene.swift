a5f00bd6b9c1f6114a08d29d2b12e8cf156d3eb1
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-02 22:20:31 +0200

    Initialize the work of loadLevel() method, so now it's time to replace comments with code

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -11,4 +11,33 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
     }
+    
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Couldn't find level1.txt in the app bundle.")
+        }
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Couldn't load level1.txt from the app bundle.")
+        }
+        
+        let lines = levelString.split(separator: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    // Load wall
+                } else if letter == "v" {
+                    // Load vortex
+                } else if letter == "s" {
+                    // Load star
+                } else if letter == "f" {
+                    // Load finish point
+                } else {
+                    fatalError("Unkown level letter \(letter)")
+                }
+            }
+        }
+    }
 }
