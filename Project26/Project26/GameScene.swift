586ff27e484e6519c660e78426934942631f1098
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-02 21:13:29 +0200

    Initialize project 26

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Usama Fouad on 02/02/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
