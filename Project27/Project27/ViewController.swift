8a583ef37d989820a764de6ed90f265a2091f311
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-03 21:45:51 +0200

    Connect button and imageView to the code

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
+    func drawRectangle() {
+        
+    }
+    
 }
 
