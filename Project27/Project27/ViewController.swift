40e699c53d1a313b6c6a12b6213dd0b60ee521a3
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-03 23:19:21 +0200

    Use lines to make an awesome shape

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -32,6 +32,8 @@ class ViewController: UIViewController {
             drawCheckerboard()
         case 3:
             drawRotetedSquares()
+        case 4:
+            drawLines()
         default:
             break
         }
@@ -105,6 +107,32 @@ class ViewController: UIViewController {
             
             ctx.cgContext.addEllipse(in: rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = image
+    }
+    
+    func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                ctx.cgContext.rotate(by: .pi / 2)
+                
+                if first {
+                    ctx.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                length *= 0.99
+            }
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
         }
         imageView.image = image
     }
