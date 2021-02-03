246112a43dac4fb06ef0965a79897f34b63e06ef
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-03 22:25:09 +0200

    Draw a rectangle

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var currentDrawType = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        drawRectangle()
     }
 
     @IBAction func redrawTapped(_ sender: Any) {
@@ -31,7 +32,19 @@ class ViewController: UIViewController {
     }
     
     func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
         
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = image
     }
     
 }
