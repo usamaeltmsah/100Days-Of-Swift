6830aeeefe1954d9ee4d4031ddd0d2da8d7dc817
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-06 20:44:07 +0200

    Initialize Projects 25-27 Consolidation project

---
+++ Projects25-27-Consolidation/Projects25-27-Consolidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects25-27-Consolidation
+//
+//  Created by Usama Fouad on 06/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
