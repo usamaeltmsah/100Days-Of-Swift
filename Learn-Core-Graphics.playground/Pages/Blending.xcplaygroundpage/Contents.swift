d0f2487ae2d7f736093a9988af4fd8b238d5c107
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-07 23:55:27 +0200

    Use blend common drawing pieces using XOR

--- Learn-Core-Graphics.playground/Pages/Blending.xcplaygroundpage/Contents.swift
+++ Learn-Core-Graphics.playground/Pages/Blending.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ let rect = CGRect(x: 0, y: 0, width: 1000, height: 1000)
 let renderer = UIGraphicsImageRenderer(bounds: rect)
 
 let rendered = renderer.image { ctx in
-    // ctx.cgContext.setBlendMode(.xor)
+    ctx.cgContext.setBlendMode(.xor)
 
     UIColor.red.setFill()
     ctx.cgContext.fillEllipse(in: CGRect(x: 200, y: 200, width: 400, height: 400))
