224931313d7dd4c6181df4dec94ee7d4cf1a6b09
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-07 21:19:46 +0200

    Draw flower using 5 circles

--- Learn-Core-Graphics.playground/Pages/Flowers.xcplaygroundpage/Contents.swift
+++ Learn-Core-Graphics.playground/Pages/Flowers.xcplaygroundpage/Contents.swift
@@ -5,14 +5,30 @@
 //: - Experiment: Your designer has provided a sketch of what they want: four red circles, with a black circle in the middle. Can you make this happen using ellipses? They've drawn the first one for you, but it isn't quite right.
 import UIKit
 
-let rect = CGRect(x: 0, y: 0, width: 1000, height: 1000)
+let rect = CGRect(x: 100, y: 100, width: 1000, height: 1000)
 let renderer = UIGraphicsImageRenderer(bounds: rect)
 
 let rendered = renderer.image { ctx in
 	// "This doesn't seem right…" – Designer
     UIColor.red.setFill()
-    let circle1 = CGRect(x: 0, y: 0, width: 500, height: 500)
+    let circle1 = CGRect(x: 100, y: 100, width: 500, height: 500)
     ctx.cgContext.fillEllipse(in: circle1)
+    
+    UIColor.red.setFill()
+    let circle2 = CGRect(x: 400, y: 100, width: 500, height: 500)
+    ctx.cgContext.fillEllipse(in: circle2)
+    
+    UIColor.red.setFill()
+    let circle3 = CGRect(x: 100, y: 400, width: 500, height: 500)
+    ctx.cgContext.fillEllipse(in: circle3)
+    
+    UIColor.red.setFill()
+    let circle4 = CGRect(x: 400, y: 400, width: 500, height: 500)
+    ctx.cgContext.fillEllipse(in: circle4)
+    
+    UIColor.black.setFill()
+    let circle5 = CGRect(x: 400, y: 400, width: 200, height: 200)
+    ctx.cgContext.fillEllipse(in: circle5)
 }
 
 showOutput(rendered)
