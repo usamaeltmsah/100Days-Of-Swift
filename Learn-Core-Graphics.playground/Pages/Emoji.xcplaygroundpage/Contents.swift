dfef430d85c2b74273fac5419ff2e6716dbccd92
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-07 23:13:55 +0200

    Draw WOW Emoji!

--- Learn-Core-Graphics.playground/Pages/Emoji.xcplaygroundpage/Contents.swift
+++ Learn-Core-Graphics.playground/Pages/Emoji.xcplaygroundpage/Contents.swift
@@ -13,8 +13,23 @@ let rendered = renderer.image { ctx in
     UIColor.yellow.setFill()
     ctx.cgContext.setLineWidth(10)
 
-    let face = CGRect(x: 0, y: 0, width: 800, height: 800)
+    let face = CGRect(x: 100, y: 100, width: 800, height: 800)
     ctx.cgContext.addEllipse(in: face)
+    ctx.cgContext.drawPath(using: .fillStroke)
+    
+    UIColor.black.setFill()
+    let eye1 = CGRect(x: 250, y: 300, width: 150, height: 150)
+    ctx.cgContext.addEllipse(in: eye1)
+    
+    let eye2 = CGRect(x: 600, y: 300, width: 150, height: 150)
+    ctx.cgContext.addEllipse(in: eye2)
+    
+    ctx.cgContext.drawPath(using: .fillStroke)
+    
+    
+    UIColor.brown.setFill()
+    let mouth = CGRect(x: 350, y: 500, width: 300, height: 300)
+    ctx.cgContext.addEllipse(in: mouth)
     ctx.cgContext.drawPath(using: .fillStroke)
 }
 
