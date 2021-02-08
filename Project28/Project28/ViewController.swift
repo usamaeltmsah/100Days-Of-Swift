81a8e45825ff943f7fac1f0f727917be0534814e
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-08 18:42:50 +0200

    Add KeychainWrapper files

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
