38ff798253b4416d54a57251ebb6c327fdd61a2e
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-08 23:06:00 +0200

    Add comments to authenticateTapped() method

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -27,17 +27,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
+        // Touch ID and Face ID are part of the Local Authentication framework
         let context = LAContext()
         var error: NSError?
         
+        // 1. Check whether the device is capable of supporting biometric authentication – that the hardware is available and is configured by the user.
+        // MARK: &error: The LocalAuthentication framework uses the Objective-C approach to reporting errors back to us, which is where the NSError type comes from – where Swift likes to have an enum that conforms to the Error protocol, Objective-C had a dedicated NSError type for handling errors.
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             let reason = "Identify yourself!"
+            // 2. If so, request that the biometry system begin a check now, giving it a string containing the reason why we're asking. For Touch ID the string is written in code; for Face ID the string is written into our Info.plist file.
             
+                // Instead what we use is the Objective-C equivalent of Swift’s inout parameters: we pass an empty NSError variable into our call to canEvaluatePolicy(), and if an error occurs that error will get filled with a real NSError instance telling us what went wrong.
+                // Objective-C’s equivalent to inout is what’s called a pointer, so named because it effectively points to a place in memory where something exists rather us passing around the actual value instead. If we had passed error into the method, it would mean “here’s the error you should use.” By passing in &error – Objective-C’s equivalent of inout – it means “if you hit an error, here’s the place in memory where you should store that error so I can read it.”
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {[weak self] success,authenticationError in
                 DispatchQueue.main.async {
+                    // when we're told whether Touch ID/Face ID was successful or not, it might not be on the main thread. This means we need to use async() to make sure we execute any user interface code on the main thread.
                     if success {
+                        // 3. If we get success back from the authentication request it means this is the device's owner so we can unlock the app.
                         self?.unlockSecretMessage()
                     } else {
+                        // 4. otherwise we show a failure message.
                         let ac = UIAlertController(title: "Authentication failed!", message: "You couldn't be verified; please try again.", preferredStyle: .alert)
                         ac.addAction(UIAlertAction(title: "OK", style: .default))
                         self?.present(ac, animated: true)
