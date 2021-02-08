5c14c1b7440408d495026b9caa0f8aa841340195
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-08 15:26:25 +0200

    Design the ui

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var secret: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func authenticateTapped(_ sender: Any) {
+    }
+    
 }
 
