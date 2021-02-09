2f48e2b293fe022a6d61849e17c7a131d2853843
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-09 17:26:11 +0200

    Initialize Project 29 (Exploding Monkeys)

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Usama Fouad on 09/02/2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
