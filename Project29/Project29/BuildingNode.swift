f61b94ca842f039e1d533068bf57ef773062b359
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-09 20:55:27 +0200

    Setup BuildingNode

--- Project29/Project29/BuildingNode.swift
+++ Project29/Project29/BuildingNode.swift
@@ -9,5 +9,21 @@ import SpriteKit
 import UIKit
 
 class BuildingNode: SKSpriteNode {
-
+    var currentImage: UIImage!
+    
+    func setup() {
+        name = "building"
+        
+        currentImage = drawBuilding(size: size)
+        texture = SKTexture(image: currentImage)
+        
+        configurePhysics()
+    }
+    
+    func configurePhysics() {
+        physicsBody = SKPhysicsBody(texture: texture!, size: size)
+        physicsBody?.isDynamic = false
+        physicsBody?.categoryBitMask = CollisionTypes.building.rawValue
+        physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+    }
 }
