b11c7be1c37e1c8be5401e0499fd4b82e15d8494
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-16 02:21:03 +0200

    Add cards to collection view cells

---
+++ Projects28-30-Consolidation/Projects28-30-Consolidation/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  Projects28-30-Consolidation
+//
+//  Created by Usama Fouad on 16/02/2021.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    @IBOutlet var cardContext: UILabel!
+    
+}
--- Projects28-30-Consolidation/Projects28-30-Consolidation/ViewController.swift
+++ Projects28-30-Consolidation/Projects28-30-Consolidation/ViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-    var cards = [CardsPair]()
+    var cardsPairs = [CardsPair]()
+    var allCards = [Card]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        cards.append(CardsPair(card: Card(context: "France"), matching: Card(context: "Paris")))
-        cards.append(CardsPair(card: Card(context: "😂")))
+        cardsPairs.append(CardsPair(card: Card(context: "France"), matching: Card(context: "Paris")))
+        cardsPairs.append(CardsPair(card: Card(context: "😂")))
+        cardsPairs.append(CardsPair(card: Card(context: "👨‍👨‍👦")))
+        cardsPairs.append(CardsPair(card: Card(context: "Egypt"), matching: Card(context: "Cairo")))
+        
+        for pair in cardsPairs {
+            allCards.append(pair.card)
+            allCards.append(pair.matching)
+        }
+        
+        allCards.shuffle()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allCards.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Card", for: indexPath) as? CardCell else {
+            fatalError("Can't create a card")
+        }
+        let card = allCards[indexPath.item]
+        cell.cardContext.text = card.context
+        
+        if card.context.count < 2 {
+            cell.cardContext.font = UIFont.systemFont(ofSize: 100)
+        }
+        
+        return cell
     }
 
 
